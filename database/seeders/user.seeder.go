b5b13d05974bbfbc678a2b0c187fa6134827de5a
Mostafa <ubirockteam@gmail.com>
2023-03-14 00:11:23 +0330

    add minor improvements

---
+++ database/seeders/user.seeder.go
@@ -0,0 +1,15 @@
+package seeders
+
+import (
+	D "docker/database"
+	M "docker/models"
+)
+
+func UserSeeder() {
+	D.DB().Create(&M.User{
+		Name:     "مصطفی",
+		Email:    "mostafa@gmail.com",
+		Password: "This is my password",
+		Role:     1,
+	})
+}
--- models/user.model.go
+++ models/user.model.go
@@ -50,19 +50,6 @@ type UserResponse struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-// func FilterUserRecord(user *User) UserResponse {
-// 	return UserResponse{
-// 		ID:        *user.ID,
-// 		Name:      user.Name,
-// 		Email:     user.Email,
-// 		Role:      *user.Role,
-// 		Photo:     *user.Photo,
-// 		Provider:  *user.Provider,
-// 		CreatedAt: *user.CreatedAt,
-// 		UpdatedAt: *user.UpdatedAt,
-// 	}
-// }
-
 var validate = validator.New()
 
 // ! not been used
