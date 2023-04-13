93446120b598ac92946ab5400a406808449cb6ed
Mostafa <ubirockteam@gmail.com>
2023-04-13 12:52:44 +0430

    cleaning and add ignore id to editUser

--- controllers/admin/admin.user.go
+++ controllers/admin/admin.user.go
@@ -3,32 +3,22 @@ package admin
 import (
 	D "docker/database"
 	F "docker/database/filters"
+	M "docker/models"
 	U "docker/utils"
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
-
-	C "docker/controllers"
-	// "strconv"
-
-	// F "docker/database/filters"
-	M "docker/models"
+	"strings"
 )
 
 // ############################
 // ##########    USER   #############
 // ############################
-// api/admin/users/1
 
 // ! Index User with admin/users route
 func IndexUser(c *fiber.Ctx) error {
 	user := []M.User{}
-	pagination := new(U.Pagination)
-	if err := c.QueryParser(pagination); err != nil {
-		U.ResErr(c, err.Error())
-	}
-	D.DB().Where("id > ?", 0).Scopes(
+	pagination := U.ParsedPagination(c)
+	D.DB().Scopes(
 		F.FilterByType(c,
 			F.FilterType{QueryName: "name", Operator: "LIKE"},
 			F.FilterType{QueryName: "nationalCode", ColumnName: "national_code"},
@@ -47,23 +37,22 @@ func IndexUser(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"meta": pagination,
 		"data": pass_data,
-	})
+	}) // same as return U.ResWithPagination(c, pass_data, *pagination)
 }
 func CheckPasswordHash(password string, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
 }
 func EditUser(c *fiber.Ctx) error {
-
 	user := M.User{}
 	payload := new(M.EditInput)
 	if err := c.BodyParser(payload); err != nil {
 		U.ResErr(c, err.Error())
 	}
-	if errs := U.Validate(payload); errs != nil {
+	if errs := U.Validate(payload, c.Params("id")); errs != nil {
 		return c.Status(400).JSON(fiber.Map{"errors": errs})
 	}
-	result1 := D.DB().Where("id = ?", c.Params("id")).Find(&user)
+	result1 := D.DB().Where("id = ?", c.Params("id")).First(&user)
 	if result1.Error != nil {
 		return U.DBError(c, result1.Error)
 	}
@@ -82,16 +71,14 @@ func EditUser(c *fiber.Ctx) error {
 	if result.Error != nil {
 		return U.DBError(c, result.Error)
 	}
-	return c.JSON(fiber.Map{
-		"message": "کاربر ویرایش شد",
-	})
+	return U.ResMessage(c, "کاربر ویرایش شد")
 }
 
 // ! user by id with admin/users/{id}
 func UserByID(c *fiber.Ctx) error {
 	user := M.User{}
-	D.DB().Where("id = ?", c.Params("id")).Find(&user)
-	if user.Name == "" {
+	result := D.DB().Where("id = ?", c.Params("id")).Find(&user)
+	if result.RowsAffected == 0 { // can check the same condition with user.Name == ""
 		return U.ResErr(c, "کاربر وجود ندارد")
 	}
 	minUser := M.MinUser{
@@ -115,43 +102,32 @@ func DeleteUser(c *fiber.Ctx) error {
 	if result.RowsAffected == 0 {
 		return U.ResErr(c, "کاربر یافت نشد")
 	}
-	return c.JSON(fiber.Map{
-		"message": " کابر با موفقیت حذف شد",
-	})
+	return U.ResMessage(c, "کاربر حذف شد")
 }
 
-// ! create two routes for verification, one for verify, second for unverify
-// ! there's no need to convert formValue to int
-// ! for verifying, you need to get the user first, if it was verified already, return error
 func UserVerification(c *fiber.Ctx) error {
 	result := D.DB().Model(&M.User{}).Where("id = ?", c.Params("id")).Update("verified", true)
 	if result.Error != nil {
 		U.DBError(c, result.Error)
 	}
-	return c.Status(400).JSON(fiber.Map{
-		"message": " بروز رسانی با موفقیت انجام شد",
-	})
+	return U.ResMessage(c, "کاربر تایید شد")
 }
 func UserUnVerification(c *fiber.Ctx) error {
-
 	result := D.DB().Model(&M.User{}).Where("id = ?", c.Params("id")).Update("verified", false)
 	if result.Error != nil {
 		U.DBError(c, result.Error)
 	}
-	return c.Status(400).JSON(fiber.Map{
-		"message": " بروز رسانی با موفقیت انجام شد",
-	})
+	return U.ResMessage(c, "کاربر رد شد")
 }
 func AddUser(c *fiber.Ctx) error {
 	payload := new(M.SignUpInput)
 	// ! parse body
-	res, err := C.BodyParserHandle(c, payload)
-	if err != nil {
-		return res
+	if err := c.BodyParser(payload); err != nil {
+		return U.ResErr(c, err.Error())
 	}
 	// ! validate request
-	if err != nil {
-		return C.ValidationHandle(c, err)
+	if errs := U.Validate(payload, c.Params("id")); errs != nil {
+		return U.ResValidationErr(c, errs)
 	}
 	// hash the password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
@@ -165,11 +141,9 @@ func AddUser(c *fiber.Ctx) error {
 		PersonalCode: payload.PersonalCode,
 		NationalCode: payload.NationalCode,
 	}
-	// ! add user to the database
 	result := D.DB().Create(&newUser)
-	// ! if any error exist in the create process, write the error
 	if result.Error != nil {
 		return U.DBError(c, result.Error)
 	}
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "user has been created successfully"})
+	return U.ResMessage(c, "کاربر ایجاد شد") // ! TODO talk with mohsen: should i send statusCreated or 200 ?
 }
