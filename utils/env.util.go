9b2245e81384de21a9379bfa244c661c6a03c685
mostafa-binesh <ubirockteam@gmail.com>
2024-02-10 12:50:41 +0330

    add EnvToBool and EnvToInt functions to env utility

--- utils/env.util.go
+++ utils/env.util.go
@@ -3,11 +3,13 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	env "github.com/joho/godotenv"
 )
+
 // returns envoirment variable if exist in the .env file
-// otherwise, try to get the env. variable from host 
+// otherwise, try to get the env. variable from host
 // if there was no env. variable, panics
 func Env(key string) string {
 	// load .env file
@@ -20,4 +22,22 @@ func Env(key string) string {
 		return value
 	}
 	return os.Getenv(key)
+}
+func EnvToBool(key string) bool {
+	value := Env(key) // Use your Env function to get the value as a string
+	if value == "true" {
+		return true
+	}
+	if value == "false" {
+		return false
+	}
+	panic(fmt.Sprintf("env variable not a bool: %s", key))
+}
+func EnvToInt(key string) int {
+	value := Env(key) // Use your Env function to get the value as a string
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		panic(fmt.Sprintf("can't convert env variable to int: %s, error: %v", key, err))
+	}
+	return intValue
 }
